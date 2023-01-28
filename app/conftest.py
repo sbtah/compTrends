39ace8b1e5102b732ca73772c19a9162eb3228ae
Sbtah <shisastore@protonmail.com>
2023-01-28 21:56:22 +0100

    Added fixtures for products.

--- app/conftest.py
+++ app/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from stores.models import EccommerceStore, LocalStore
 from categories.models import Category
+from products.models import Product, ProductLocalData, ProductExtraField
 
 
 @pytest.fixture
@@ -31,4 +32,37 @@ def example_category(example_eccommerce_store):
         name="Test Category",
         url="http://example-store.com/test-category/",
         parrent_store=e_store,
+    )
+
+
+@pytest.fixture
+def example_product(example_category, example_eccommerce_store):
+    e_store = example_eccommerce_store
+    category = example_category
+    return Product.objects.create(
+        name="Test Product",
+        url="http://example.com/test-product-1",
+        parrent_store=e_store,
+        parrent_category=category,
+    )
+
+
+@pytest.fixture
+def example_product_local_data(example_product, example_local_store):
+    product = example_product
+    local_store = example_local_store
+    return ProductLocalData.objects.create(
+        parrent_product=product,
+        parrent_local_store=local_store,
+        name="Test Product",
+    )
+
+
+@pytest.fixture
+def example_product_extra_field(example_product):
+    product = example_product
+    return ProductExtraField.objects.create(
+        parrent_product=product,
+        field_name="Test Field",
+        field_data="Test Field Data...",
     )
