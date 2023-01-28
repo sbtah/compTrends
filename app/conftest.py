46b4ddc6f7d0c59328053d4f714f3c2d1f9c4257
Sbtah <shisastore@protonmail.com>
2023-01-28 11:59:04 +0100

    Added example_local_store fixture.

--- app/conftest.py
+++ app/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from stores.models import EccommerceStore
+from stores.models import EccommerceStore, LocalStore
 
 
 @pytest.fixture
@@ -9,4 +9,15 @@ def example_eccommerce_store():
         main_url="https://teststore.com/",
         module_name="test_store",
         class_name="TestStore",
+    )
+
+
+@pytest.fixture
+def example_local_store(example_eccommerce_store):
+    e_store = example_eccommerce_store
+    return LocalStore.objects.create(
+        parrent_store=e_store,
+        name="Local Store 1",
+        scraped_id=1,
+        is_active=True,
     )
