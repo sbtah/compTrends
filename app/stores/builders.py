f3cc4770c76231ea8bbfda63efa52d012f322e8d
Sbtah <shisastore@protonmail.com>
2023-01-30 17:47:10 +0100

    Modified api_update_or_create_local_store to save date of scrape.

--- app/stores/builders.py
+++ app/stores/builders.py
@@ -6,6 +6,7 @@ def api_update_or_create_local_store(
     parrent_eccomerce_store,
     name,
     scraped_id,
+    last_scrape,
     **kwargs,
 ):
     """Update or create LocalStore object with data from API call."""
@@ -14,15 +15,18 @@ def api_update_or_create_local_store(
         local_store = LocalStore.objects.get(scraped_id=scraped_id)
         if local_store.name != name:
             local_store.name = name
+            local_store.last_scrape = last_scrape
             local_store.save()
             logger.info(f"Updated LocalStore: {local_store}")
         else:
+            local_store.last_scrape = last_scrape
             logger.info(f"No Updates for LocalStore: {local_store}. Passing...")
     except LocalStore.DoesNotExist:
         local_store = LocalStore.objects.create(
             parrent_store=parrent_eccomerce_store,
             name=name,
             scraped_id=scraped_id,
+            last_scrape=last_scrape,
             **kwargs,
         )
         logger.info(f"Created LocalStore: {local_store}")
