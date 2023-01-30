d511910078bc876a1a838819c7dbeb4540ef420d
Sbtah <shisastore@protonmail.com>
2023-01-30 17:50:02 +0100

    Added category app models to admin. Also added custom admin for Category object.

--- app/categories/admin.py
+++ app/categories/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from categories import models
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Custom admin for Category object."""
+
+    list_filter = (
+        "name",
+        "scraped_id",
+        "category_level",
+        "is_active",
+        "last_scrape",
+    )
+    list_display = (
+        "name",
+        "scraped_id",
+        "category_level",
+        "is_active",
+        "last_scrape",
+    )
+    search_fields = [
+        "name",
+        "scraped_id",
+        "category_level",
+        "is_active",
+        "last_scrape",
+    ]
+
+
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.CategoryExtraField)
