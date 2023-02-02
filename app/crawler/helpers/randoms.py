499921ed2cd30186c2d252d5ae2d0a138988556e
Sbtah <shisastore@protonmail.com>
2023-02-02 22:13:04 +0100

    Increased time in random sleep since it only being used in long get requests.

--- app/crawler/helpers/randoms.py
+++ app/crawler/helpers/randoms.py
@@ -6,6 +6,6 @@ from utilites.logger import logger
 def random_sleep_small():
     """Custom sleep function that sleeps from 3 to 6 seconds"""
 
-    value = randint(3, 5)
+    value = randint(10, 15)
     logger.info(f"Random sleep for: {value} seconds.")
     return time.sleep(value)
