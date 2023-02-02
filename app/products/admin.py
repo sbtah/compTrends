8ecd96a8caff5cb6d03230e673d7becbbedb3a47
Sbtah <shisastore@protonmail.com>
2023-02-02 21:57:55 +0100

    Added Product model to admin. Enabled search and filtering.

--- app/products/admin.py
+++ app/products/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from products.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_filter = (
+        "is_active",
+        "parrent_category",
+    )
+    list_display = (
+        "name",
+        "scraped_id",
+        "parrent_category",
+        "sku",
+        "ean",
+        "brand_name",
+    )
+    search_fields = [
+        "name",
+        "scraped_id",
+        "parrent_category__name",
+        "sku",
+        "ean",
+        "brand_name",
+    ]
+
+
+admin.site.register(Product, ProductAdmin)
