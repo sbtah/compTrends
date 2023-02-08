b20576fa418186b82a881a3a37391c220075c2e0
Sbtah <shisastore@protonmail.com>
2023-02-08 07:33:28 +0100

    Added api_scrape_stores management command.

---
+++ app/core/management/commands/api_scrape_stores.py
@@ -0,0 +1,34 @@
+from crawler.logic.data_scraper import DataApiScraper
+from django.core.management.base import BaseCommand
+from stores.models import LocalStore
+
+
+class Command(BaseCommand):
+    """Main class for command object that imports LocalStores from Api."""
+
+    def handle(self, *args, **options):
+        """Custom handle method."""
+
+        number_of_local_stores_start = LocalStore.objects.all().count()
+        self.stdout.write(
+            self.style.WARNING(
+                f"""
+                Discovery of LocalStores started ...
+                Current number of LocalStores: {number_of_local_stores_start}
+                """
+            )
+        )
+
+        DataApiScraper().scrape_local_stores()
+
+        number_of_local_stores_finish = LocalStore.objects.all().count()
+        new_stores = number_of_local_stores_finish - number_of_local_stores_start
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"""
+                Discovery of LocalStores finished.
+                Current number of LocalStores: {number_of_local_stores_finish}
+                Found: {new_stores} new LocalStores since last discovery process
+                """
+            )
+        )
