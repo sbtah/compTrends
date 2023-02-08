75509d263b0c4916d8827ed00be251f2288138ad
Sbtah <shisastore@protonmail.com>
2023-02-08 08:30:12 +0100

    Update validate_products management command.

--- app/core/management/commands/api_scrape_data.py
+++ app/core/management/commands/api_scrape_data.py
@@ -1,4 +1,5 @@
 from categories.models import Category
+from crawler.logic.data_scraper import DataApiScraper
 from django.core.management.base import BaseCommand
 from products.models import Product, ProductLocalData
 from stores.models import LocalStore
@@ -13,6 +14,7 @@ class Command(BaseCommand):
         number_of_local_stores_start = LocalStore.objects.all().count()
         number_of_categories_start = Category.objects.all().count()
         number_of_products_start = Product.objects.all().count()
+        number_of_products_local_data_start = ProductLocalData.objects.all().count()
 
         self.stdout.write(
             self.style.WARNING(
@@ -24,13 +26,21 @@ class Command(BaseCommand):
                 Current number of LocalStores: {number_of_local_stores_start}
                 Current number of Categories: {number_of_categories_start}
                 Current number of Products: {number_of_products_start}
+                Current number of ProductLocalDatas: {number_of_products_local_data_start}
                 """
             )
         )
+        DataApiScraper.scrape_local_stores()
+        DataApiScraper.scrape_main_categories()
+        DataApiScraper.scrape_child_categories()
+        DataApiScraper.discover_and_scrape_products_by_id()
+        DataApiScraper.validate_products_urls()
+        DataApiScraper.scrape_product_local_data()
 
         number_of_categories_finish = Category.objects.all().count()
         number_of_products_finish = Product.objects.all().count()
         number_of_local_stores_finish = LocalStore.objects.all().count()
+        number_of_products_local_data_finish = ProductLocalData.objects.all().count()
 
         self.stdout.write(
             self.style.SUCCESS(
@@ -39,6 +49,7 @@ class Command(BaseCommand):
                 Current number of LocalStores: {number_of_local_stores_finish}
                 Current number of Categories: {number_of_categories_finish}
                 Current number of Products: {number_of_products_finish}
+                Current number of ProductLocalDatas: {number_of_products_local_data_finish}
                 """
             )
         )
