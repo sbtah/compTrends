5f9ee44b84bfab5a6c6d896fdbde725a5414306f
Sbtah <shisastore@protonmail.com>
2023-02-18 09:49:19 +0100

    Added query_debugger function within utilites. This will be used to debug query speeds.

---
+++ app/utilites/debuger.py
@@ -0,0 +1,25 @@
+from django.db import connection, reset_queries
+import time
+import functools
+from utilites.logger import logger
+
+
+def query_debugger(func):
+    @functools.wraps(func)
+    def inner_func(*args, **kwargs):
+        reset_queries()
+
+        start_queries = len(connection.queries)
+
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+
+        end_queries = len(connection.queries)
+
+        logger.info(f"Function : {func.__name__}")
+        logger.info(f"Number of Queries : {end_queries - start_queries}")
+        logger.info(f"Finished in : {(end - start):.2f}s")
+        return result
+
+    return inner_func
